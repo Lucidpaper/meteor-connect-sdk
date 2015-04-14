9ca51f9baba9cab344794d23f7cb552f0d17f920
Aiko <lucidpaper@gmail.com>
2015-04-14 12:58:17 -0600

    added phonegap plugin github tarball

--- package.js
+++ package.js
@@ -1,16 +1,20 @@
 Package.describe({
   name: 'lucidpaper:connectsdk',
   version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  summary: 'A meteor wrapper of the Connect SDK',
+  git: 'https://github.com/Lucidpaper/meteor-connect-sdk',
   documentation: 'README.md'
 });
 
+// add phonegap depends here
+Cordova.depends({
+  // "org.apache.cordova.camera":"0.3.2"
+  'com.connectsdk.plugins.connectsdk':'https://github.com/ConnectSDK/Connect-SDK-Cordova-Plugin/tarball/01daa03aca52f06803ab954d592e241da6cb238c'
+});
+
 Package.onUse(function(api) {
+  api.export('ConnectSDK');
+  api.use(["templating"]);
   api.versionsFrom('1.1.0.2');
   api.addFiles('connectsdk.js');
 });
